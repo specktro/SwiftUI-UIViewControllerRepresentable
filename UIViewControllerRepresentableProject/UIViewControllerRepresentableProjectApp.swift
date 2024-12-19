fb1f05a1e23389aab4d778a495eea635f0fefb03
@specktro <specktro@nonull.mx>
2024-12-19 14:41:44 -0600

    chore: Add initial commit with gitignore file from GIST

---
+++ UIViewControllerRepresentableProject/UIViewControllerRepresentableProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerRepresentableProjectApp.swift
+//  UIViewControllerRepresentableProject
+//
+//  Created by specktro on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct UIViewControllerRepresentableProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
