c570fc8241c82a0e9c67821c4b551c916e1a206f
@specktro <specktro@nonull.mx>
2024-12-19 19:27:51 -0600

    chore: Add Image with default SFF photo symbol

--- UIViewControllerRepresentableProject/ContentView.swift
+++ UIViewControllerRepresentableProject/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showImagePickerView: Bool = false
+    @State private var selectedImage: UIImage?
+    
+    var imageView: Image {
+        guard let selectedImage = selectedImage else {
+            return Image(systemName: "photo")
+        }
+        
+        return Image(uiImage: selectedImage)
+    }
+    
     var body: some View {
         VStack {
             Text("Choose and Image!")
                 .font(.largeTitle)
                 .padding()
             Spacer()
-            Image(systemName: "photo")
+            imageView
                 .resizable()
                 .scaledToFit()
                 .onTapGesture {
